16b891350d991f501f800323ae9a71a2e990214b
mokha <mokha@mokha.mokha.io>
2019-06-03 18:00:07 +0300

    signup form

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,8 @@
 from django.urls import reverse
+from django import forms
 from django.utils.translation import ugettext as _
 
-from allauth.account.forms import LoginForm
+from allauth.account.forms import LoginForm, SignupForm
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import HTML
@@ -40,3 +41,51 @@ class CustomLoginForm(LoginForm):
         self.helper.form_class = 'form-signin'
         self.helper.label_class = 'sr-only'
         self.helper.field_class = ''
+
+
+class CustomSignupForm(SignupForm):
+    field_order = [
+        'first_name',
+        'last_name',
+        'email',
+        'email2',  # ignored when not present
+        'username',
+        'password1',
+        'password2'  # ignored when not present
+    ]
+
+    first_name = forms.CharField(max_length=30, label=_('First name'),
+                                 widget=forms.TextInput(attrs={'placeholder': _('First name')}))
+    last_name = forms.CharField(max_length=30, label=_('Last name'),
+                                widget=forms.TextInput(attrs={'placeholder': _('Last name')}))
+
+    def __init__(self, *args, **kwargs):
+        super(CustomSignupForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        # Add magic stuff to redirect back.
+        self.helper.layout.append(
+            HTML(
+                "{% if redirect_field_value %}"
+                "<input type='hidden' name='{{ redirect_field_name }}'"
+                " value='{{ redirect_field_value }}' />"
+                "{% endif %}"
+            )
+        )
+        # Add submit button like in original form.
+        self.helper.layout.append(
+            HTML(
+                '<button class="btn btn-lg btn-primary btn-block" type="submit">'
+                '%s</button>' % _('Signup')
+            )
+        )
+
+        self.helper.form_class = 'form-signin'
+        self.helper.label_class = 'sr-only'
+        self.helper.field_class = ''
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user

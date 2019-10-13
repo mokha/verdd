241e63702b5c0abd584e29deb6161d3a81318403
Hämäläinen, Mika K <mika.hamalainen@helsinki.fi>
2019-10-13 22:16:01 +0300

    prööt

--- manageXML/management/commands/import_csv.py
+++ manageXML/management/commands/import_csv.py
@@ -195,7 +195,27 @@ def add_termwiki():
                 print(e)
 
 def add_deriv():
-    term_json = json_load(script_path("../../../additional_data/sms_termwiki.json"))
+    deriv_json = json_load(script_path("../../../additional_data/sms_deriv.json"))
+    for word1, derv_data in deriv_json.items():
+        id, word = word1.split("_")
+        try:
+            l1 = Lexeme.objects.get(id=id, language="sms")
+            for derivation in derv_data:
+                l2 = Lexeme.objects.get(id=derivation[0].split("_")[0], language="sms")
+                if "+Cmp" in derivation[1]:
+                    rel_type = 2
+                else:
+                    rel_type = 3
+                print(l1)
+                print(l2)
+                r, c = Relation.objects.get_or_create(type=rel_type, lexeme_from_id=l2.id, lexeme_to_id=l1.id, notes=derivation[1])
+                r.save()
+        except Exception as e:
+                print(e)
+
+
+
+
 
 def process_row(row, df, lang_source, lang_target):
     row_dict = dict(row)

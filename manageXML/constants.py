27295dedf5ca1324e6c97e04a5a88b5ff96d6cbd
Khalid Alnajjar <dr.mokha@gmail.com>
2020-02-07 13:28:41 +0200

    added phraseology relation

--- manageXML/constants.py
+++ manageXML/constants.py
@@ -21,12 +21,14 @@ ETYMOLOGY = 1
 COMPOUND = 2
 DERIVATION = 3
 VARIATION = 4
+PHRASEOLGY = 5
 RELATION_TYPE_OPTIONS = (
     (TRANSLATION, _('Translation')),
     (ETYMOLOGY, _('Etymology')),
     (COMPOUND, _('Compound')),
     (DERIVATION, _('Derivation')),
     (VARIATION, _('Variation')),
+    (PHRASEOLGY, _('Phraseolgy')),
     (99, _('Other'))
 )
 RELATION_TYPE_OPTIONS_DICT = dict(RELATION_TYPE_OPTIONS)

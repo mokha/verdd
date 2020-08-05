5cefa9611f5a17f70f84d3b9b28482a4d0c948cb
mokha <mokha@mokha.mokha.io>
2020-08-05 22:47:46 +0300

    setup.sh updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@ echo -n MySQL Username:
 read mysql_user
 echo -n MySQL Password:
 read -s mysql_password
+echo
 echo -n MySQL DB:
 read database_name
 echo -n MySQL DB User:
@@ -11,11 +12,11 @@ echo
 
 ### Setup MySQL DB
 # drop existing DB
-mysql --user="$mysql_user" --password="$mysql_password" --database="$database_name" --execute="DROP DATABASE IF EXISTS $database_name;"
+mysql --user="$mysql_user" --password="$mysql_password" --execute="DROP DATABASE IF EXISTS $database_name;"
 # create new DB
-mysql --user="$mysql_user" --password="$mysql_password" --database="$database_name" --execute="CREATE DATABASE $database_name DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;"
+mysql --user="$mysql_user" --password="$mysql_password" --execute="CREATE DATABASE $database_name DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;"
 # drop existing DB
-mysql --user="$mysql_user" --password="$mysql_password" --database="$database_name" --execute="GRANT ALL PRIVILEGES ON $database_name.* TO '$database_db_user'@'localhost';"
+mysql --user="$mysql_user" --password="$mysql_password" --execute="GRANT ALL PRIVILEGES ON $database_name.* TO '$database_db_user'@'localhost';"
 
 ### Setup Ve'rdd
 python manage.py migrate         # install migrations

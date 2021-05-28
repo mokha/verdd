593cebc89a5918683eec7abdd08eccdbc0fe7b64
Khalid Alnajjar <dr.mokha@gmail.com>
2021-05-28 05:05:36 +0400

    update apertium export: fixed randomization, [l/r instead of s/t], and speed up

--- apertium/management/commands/export_dix.py
+++ apertium/management/commands/export_dix.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from zipfile import ZipFile
 import uuid
 import time
-from django.db.models.functions import Cast, Substr, Upper
+from django.db.models.functions import Cast, Substr, Upper, Concat
 from django.db.models import Prefetch, F, Value, When, Case
 from manageXML.models import *
 from manageXML.utils import *
@@ -20,87 +20,85 @@ def export_monodix(src_lang, tgt_lang, directory_path, ignore_file=None, *args, 
     raise NotImplementedError("Not implemented yet.")
 
 
-def get_relations(src_lang, tgt_lang, ignore_file=None, *args, **kwargs):
+def export_bidix(left_lang, right_lang, directory_path, ignore_file=None, *args, **kwargs):
     checked = kwargs.get('approved', None)
+    sdef_flag = kwargs.get('sdef')
 
-    relations = Relation.objects.filter(type=TRANSLATION)
+    left_relations = Relation.objects.filter(type=TRANSLATION) \
+        .filter(Q(lexeme_from__language=left_lang) & Q(lexeme_to__language=right_lang))
+    right_relations = Relation.objects.filter(type=TRANSLATION) \
+        .filter(Q(lexeme_from__language=right_lang) & Q(lexeme_to__language=left_lang))
 
     if checked is not None:
-        relations = relations.filter(checked=checked)
-
-    tgt_relations = Relation.objects.filter(type=TRANSLATION)
-
-    if checked is not None:
-        tgt_relations = tgt_relations.filter(checked=checked)
+        left_relations = left_relations.filter(checked=checked)
+        right_relations = right_relations.filter(checked=checked)
 
     if ignore_file:
         to_ignore_ids = read_first_ids_from(ignore_file)
-        relations = relations.exclude(pk__in=to_ignore_ids)
-        tgt_relations = tgt_relations.exclude(pk__in=to_ignore_ids)
+        left_relations = left_relations.exclude(pk__in=to_ignore_ids)
+        right_relations = right_relations.exclude(pk__in=to_ignore_ids)
 
-    relations = relations \
-        .filter(
-        (Q(lexeme_from__language=src_lang) & Q(lexeme_to__language=tgt_lang)) |
-        (Q(lexeme_from__language=tgt_lang) & Q(lexeme_to__language=src_lang))
-    ) \
-        .prefetch_related(
-        Prefetch('lexeme_from', queryset=Lexeme.objects.prefetch_related('miniparadigm_set')),
-        Prefetch('lexeme_to', queryset=Lexeme.objects.prefetch_related('miniparadigm_set')),
+    left_relations = left_relations.annotate(lexeme_from_to_str=Concat('lexeme_from', Value('_'), 'lexeme_to'))
+    right_relations = right_relations.annotate(lexeme_from_to_str=Concat('lexeme_to', Value('_'), 'lexeme_from'))
+
+    _lr = left_relations.values_list('lexeme_from_to_str', flat=True)
+    _rr = right_relations.values_list('lexeme_from_to_str', flat=True)
+    common_ids = set(_lr) & set(_rr)
+    common_ids_rev = {_l.split('_').reverse() for _l in common_ids}
+    right_relations = right_relations.exclude(lexeme_from_to_str__in=common_ids_rev)
+    l_ids = list(left_relations.values_list('pk', flat=True))
+    r_ids = list(right_relations.values_list('pk', flat=True))
+    _ids = l_ids + r_ids
+
+    relations = Relation.objects.filter(pk__in=_ids).prefetch_related(
+        Prefetch('lexeme_from', queryset=Lexeme.objects.prefetch_related('miniparadigm_set', 'lexememetadata_set')),
+        Prefetch('lexeme_to', queryset=Lexeme.objects.prefetch_related('miniparadigm_set', 'lexememetadata_set')),
         'relationexample_set', 'relationmetadata_set') \
         .order_by('lexeme_from__pos', 'lexeme_from__lexeme_lang') \
         .all()
 
-    tgt_relations = tgt_relations \
-        .filter(lexeme_from__language=tgt_lang, lexeme_to__language=src_lang) \
-        .values_list('lexeme_to', 'lexeme_from').all()
-    tgt_relations = set(tgt_relations)
-    src_relations = set(relations.values_list('lexeme_from', 'lexeme_to'))
-    ltr = src_relations - tgt_relations
-    rtl = tgt_relations - src_relations
-
-    covered_relations = []
-    for r in relations:
-        _key = (r.lexeme_from.id, r.lexeme_to.id,)
-        if _key in covered_relations:
-            continue
-        if _key in ltr:
-            r.dir = 'LR'
-        elif _key in rtl:
-            r.dir = 'RL'
-        covered_relations.append(_key)
-        yield r
-
-
-def export_bidix(src_lang, tgt_lang, directory_path, ignore_file=None, *args, **kwargs):
-    relations = list(get_relations(src_lang, tgt_lang, ignore_file, *args, **kwargs))
     # alphabets
-    alphabet = ''
-
+    alphabet = set()
     # sdefs
-    lexemes = Lexeme.objects.filter(Q(language=src_lang) | Q(language=tgt_lang)).prefetch_related(
-        'lexememetadata_set').all()
+    sdefs = dict()
     symbols = set()
-    for _l in lexemes.iterator():
-        symbols |= set(_l.symbols())
-    system_symbols = Symbol.all_dict()
-    sdefs = {s: system_symbols[s] for s in symbols if s in system_symbols}
+
+    for r in relations:
+        if r.pk not in common_ids:  # not bidirectional
+            if r.pk in r_ids:
+                r.lexeme_from, r.lexeme_to = r.lexeme_to, r.lexeme_from  # swap them
+                r.dir = 'RL'
+            else:
+                r.dir = 'LR'
+
+        alphabet |= set(r.lexeme_from.lexeme)
+        alphabet |= set(r.lexeme_to.lexeme)
+
+        if sdef_flag:
+            symbols |= set(r.lexeme_from.symbols()) | set(r.lexeme_to.symbols())
+
+    alphabet = ''.join(sorted(alphabet))
+
+    if sdef_flag:
+        system_symbols = Symbol.all_dict()
+        sdefs = {s: system_symbols[s] for s in symbols if s in system_symbols}
 
     # save
     xml = render_to_string("export/bidix_xml.html", {'alphabet': alphabet, 'sdefs': sdefs, 'relations': relations})
     _filename = "apertium-{}-{}.{}-{}-{}.dix".format(
-        src_lang,
-        tgt_lang,
-        src_lang,
-        tgt_lang,
+        left_lang,
+        right_lang,
+        left_lang,
+        right_lang,
         time.strftime("%Y%m%d-%H%M")
     )
     with open("{}/{}".format(directory_path, _filename), 'w', encoding='utf-8') as f:
         f.write(xml)
 
 
-def export(src_lang, tgt_lang, directory_path, ignore_file=None, *args, **kwargs):
-    export_fn = export_bidix if tgt_lang else export_monodix
-    return export_fn(src_lang, tgt_lang, directory_path, ignore_file, *args, **kwargs)
+def export(left_lang, right_lang, directory_path, ignore_file=None, *args, **kwargs):
+    export_fn = export_bidix if right_lang else export_monodix
+    return export_fn(left_lang, right_lang, directory_path, ignore_file, *args, **kwargs)
 
 
 class Command(BaseCommand):
@@ -112,14 +110,17 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('-d', '--dir', type=str, help='The directory path where to store the XMLs in.', )
-        parser.add_argument('-s', '--source', type=str, help='Three letter code of source language.', )
-        parser.add_argument('-t', '--target', type=str, nargs='?', default=None,
-                            help='Three letter code of target language.', )
+        parser.add_argument('-l', '--left', type=str, help='Three letter code of left language.', )
+        parser.add_argument('-r', '--right', type=str,
+                            nargs='?', default=None, help='Three letter code of right language.', )
         parser.add_argument('-i', '--ignore', type=str, nargs='?', default=None,
                             help='A file containing relations to be ignored. '
                                  'The first value must be the ID of the relation.', )
 
         parser.add_argument('--approved', type=lambda v: bool(strtobool(v)), nargs='?', const=True, default=None, )
+
+        parser.add_argument('--sdef', dest='sdef', action='store_true')
+        parser.set_defaults(sdef=False)
 
     def success_info(self, info):
         return self.stdout.write(self.style.SUCCESS(info))
@@ -129,8 +130,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         dir_path = options['dir']
-        src_lang = options['source']
-        tgt_lang = options['target']
+        left_lang = options['left']
+        right_lang = options['right']
         ignore_file = options['ignore']
 
         if not os.path.isdir(dir_path):
@@ -138,4 +139,4 @@ class Command(BaseCommand):
         elif ignore_file and not os.path.isfile(ignore_file):
             return self.error_info("The ignore file doesn't exist.")
 
-        export(src_lang, tgt_lang, dir_path, ignore_file, **options)
+        export(left_lang, right_lang, dir_path, ignore_file, **options)

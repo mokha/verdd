07052351a73b27cda763c63fe43b527960ed3e99
Khalid Alnajjar <dr.mokha@gmail.com>
2024-09-09 19:49:05 +0300

    load .env when using uwsgi

--- verdd/wsgi.py
+++ verdd/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+load_dotenv()
 
 env = os.getenv("DEPLOYMENT_SETTINGS", "development")
 
